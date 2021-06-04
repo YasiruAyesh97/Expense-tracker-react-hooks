48104468cf0c4f1baeb5093e5408271b534b932c
yasiru <yashiruayashmantha@gmail.com>
2021-06-04 13:58:23 +0530

    bulid Balance component

--- src/component/Balance.js
+++ src/component/Balance.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useContext} from "react";
+import {GlobalContext} from "../context/GlobalState";
+
 
 export const Balance =()=>{
+    const {transitions} =useContext(GlobalContext);
+    const amounts =transitions.map(transition =>transition.amount);
+    const total =amounts.reduce((acc,item) => (acc +=item), 0).toFixed(2);
     return(
         <>
-           <h4>Your Balance</h4>
-            <h1 id="balance">$0.00</h1>
+            <h4>Your Balance</h4>
+            <h1 id="balance">${total}</h1>
         </>
-    );
+    )
 }
